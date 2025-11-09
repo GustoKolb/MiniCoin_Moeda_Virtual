c7051979f648ba403bc81d973fa94fc0fc843800
dioni <dioni@dioni.>
2025-11-09 18:31:44 -0300

    fix

--- controllers/BlockchainWebSocket.cc
+++ controllers/BlockchainWebSocket.cc
@@ -23,37 +23,46 @@ bool checkMessageIsValid(const std::string message, const std::string prefix)
         return false;
 }
 
+double getValue(const std::string message, const std::string prefix)
+{
+    const auto rest = message.substr(prefix.length());
+
+    const auto value = std::stod(rest);
+
+    auto withdrawal = new Currency(value);
+}
+
 void BlockchainWebSocket::handleNewMessage(const WebSocketConnectionPtr &wsConnPtr, std::string &&message, const WebSocketMessageType &type)
 {
-    std::cout << "teste";
     // write your application logic here
     if (message.starts_with("deposit") && checkMessageIsValid(message, "deposit"))
     {
-        const auto value = std::stod(rest);
+        const auto value = getValue(message, "deposit");
 
         auto deposit = new Currency(value);
-        BlockChain::depositValue(deposit);
+        // BlockChain::depositValue(deposit);
     }
     else if (message.starts_with("withdraw") && checkMessageIsValid(message, "withdraw"))
     {
         /* code */
 
-        const auto value = std::stod(rest);
+        const auto value = getValue(message, "withdraw");
 
         auto withdrawal = new Currency(value);
 
-        BlockChain::CheckWithdrawal(withdrawal);
+        // BlockChain::CheckWithdrawal(withdrawal);
     }
     else
     {
-        wsConnPtr->send("Mensagem Inválida")
+        wsConnPtr->send("Mensagem Inválida");
         /* code */
     }
 }
 void BlockchainWebSocket::handleNewConnection(const HttpRequestPtr &req, const WebSocketConnectionPtr &wsConnPtr)
 {
-    //Fazer check de se lista ja existe
-    // write your application logic here
+    // Mudar para static blockchain
+    // Fazer check de se lista ja existe
+    //  write your application logic here
 }
 void BlockchainWebSocket::handleConnectionClosed(const WebSocketConnectionPtr &wsConnPtr)
 {
